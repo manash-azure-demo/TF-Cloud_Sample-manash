25569245f31e51c0fd18c3df3ca317cdb879e610
manashdas2005 <68675012+manashdas2005@users.noreply.github.com>
2024-05-29 22:18:54 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "null_resource" "tfc_test" {
+
+
+  count = 23
+
+
+
+  provisioner "local-exec" {
+
+    command = "echo 'Test ${count.index}'"
+  }
+}
+terraform {
+  cloud {
+    organization = "Manash1"
+
+    workspaces {
+      name = "TF-Cloud_Sample-manash"
+    }
+  }
+}   
